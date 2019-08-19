0b9568ed70262513f4e21757e684aca5ee298d38
BorisVassilev1 <37549016+BorisVassilev1@users.noreply.github.com>
2019-08-19 23:21:53 +0300

    Update fenwickTree.cpp

--- fenwick tree/fenwickTree.cpp
+++ fenwick tree/fenwickTree.cpp
@@ -8,31 +8,20 @@ int n = 12;
 void updateBiTree(int* biTree, int n, int i, int val) {
     ++ i;
     while( i < n +1) {
-        //cout << i << " -> ";
         biTree[i - 1] += val;
-        //cout << biTree[i-1] << " ";
         i += i & (-i);
     }
-    //cout << " -> " << val << " ";
-    //cout<< endl;
 }
 
 void constructBiTree(int* biTree,int* arr, int n) {
 
     for( int i = 0; i < n; i ++) {
         biTree[i] = 0;
-        //cout << biTree[i] << " ";
     }
-    //cout << endl << endl;
     for(int i = 0; i < n; i ++) {
         updateBiTree(biTree, n, i, arr[i]);
     }
-    for(int i = 0; i < n; i ++) {
-        cout << biTree[i] << " ";
-    }
-    cout << endl;
 
-    //return biTree;
 }
 
 
